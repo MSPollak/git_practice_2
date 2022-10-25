88365dbfe49ab126d7925ac78ab4a1eef6cd9bb5
MSPollak <baxi585@gmail.com>
2022-10-25 10:21:45 +0300

    haha

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+//   const [counter, setCounter] = useState(0);
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>Incrament</button>
-      <span>{counter}</span>
-      <button onClick={() => setCounter(counter - 1)}>Decrament</button>
+      {/* <button onClick={() => setCounter(counter + 1)}>Incrament</button> */}
+      {/* <span>{counter}</span> */}
+        <span>kjsnjgkngjdsa</span>
+      {/* <button onClick={() => setCounter(counter - 1)}>Decrament</button> */}
     </div>
   );
 }
