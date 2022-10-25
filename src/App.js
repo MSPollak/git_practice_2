11b743fd4670285876d10ef9694cf26417eabbf2
MSPollak <baxi585@gmail.com>
2022-10-25 10:08:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import Counter from './Components/Counter';
 
 function App() {
-  const [counter,setCounter] = useState(0)
   return (
     <div className="App">
-      <button onClick = {() => setCounter(counter+1)}>Incrament</button>
-        <span>{counter}</span>
-      <button onClick = {() => setCounter(counter-1)}>Decrament</button>
+      <Counter />
     </div>
   );
 }
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Incrament</button>
+      <span>{counter}</span>
+      <button onClick={() => setCounter(counter - 1)}>Decrament</button>
+    </div>
+  );
+}
+
+export default Counter;
